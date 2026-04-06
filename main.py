a63f98b7a4d96ee55aa4cfa72969f7ea31cd7495
iago monfredini <iagomonfredini46171@gmail.com>
2026-04-06 16:42:24 -0300

    Função Cadastrar Álbum✔

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Album(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String(100), nullable=False)
-    lançamento = Column(Integer, nullable=False)
+    lancamento = Column(Integer, nullable=False)
     quantidade_musicas = Column(Integer, nullable=False)
     faixa_principal = Column(String(100), nullable=False)
 
@@ -38,7 +38,7 @@ class Album(Base):
     artistas = relationship("Artista", back_populates="albuns")
 
     def __repr__(self):
-        return f"ALBUM: ID = {self.id} - NOME = {self.nome} - LANÇAMENTO = {self.lançamento} - QUANTIDADE DE MUSICAS = {self.quantidade_musicas} - FAIXA PRINCIPAL = {self.faixa_principal}"
+        return f"ALBUM: ID = {self.id} - NOME = {self.nome} - LANÇAMENTO = {self.lancamento} - QUANTIDADE DE MUSICAS = {self.quantidade_musicas} - FAIXA PRINCIPAL = {self.faixa_principal}"
    
 
 engine = create_engine("sqlite:///Entreterimento.db")
@@ -59,9 +59,31 @@ def cadastrar_artista():
             artista = Artista(nome=nome_artista, idade=idade, nacionalidade=nacionalidade,nome_artistico=nome_artistico)
             session.add(artista)
             session.commit()
-            print("Artista criado com sucesso!")
+            print("Artista cadastrado com sucesso!")
         except Exception as erro:
             session.rollback()
             print(f"Ocorreu um erro {erro}")
+    
+def cadastrar_album():
+    nome_album = input("Digite o nome do álbum: ").strip().capitalize()
+    lancamento_album = input(f"Digite o lançamento do álbum {nome_album}: ").capitalize()
+    qtd_musicas = int(input(f"Digite a quantidade de músicas do álbum {nome_album}: "))
+    faixa_principal = input(f"Digite a faixa principal do álbum {nome_album}: ").capitalize()
 
-cadastrar_artista()
+    buscar_artista = input(f"Digite o nome do artistas do álbum {nome_album}: ").strip().capitalize()
+
+    with Session() as session:
+        try:
+            #Como buscar um valor no banco usando sqlalchemy
+            albuns = session.query(Artista).filter_by(nome=buscar_artista).first()
+            if buscar_artista == None:
+                print("Não encontrei nenhum artista com esse nome.")
+                return
+            else:
+                albuns = Album(nome=nome_album, lancamento=lancamento_album, quantidade_musicas=qtd_musicas, faixa_principal=faixa_principal)
+                session.add(albuns)
+                session.commit()
+                print(f"Álbum cadastrado com sucesso!")
+        except Exception as erro:
+            session.rollback()
+            print(f"Ocorreu um erro {erro}")
